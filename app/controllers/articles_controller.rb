2bfb85f8fb4db6d463ff2f4a0784ef5cdc233e67
Abirami2k17 <abirami2k17@gmail.com>
2021-08-03 12:49:43 +0530

    Listing and new articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,25 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+
+    def index
+        @articles = Article.all
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @  = Object.new(params[: ])
+        if @ .save
+          flash[:success] = "Object successfully created"
+          redirect_to @ 
+        else
+          flash[:error] = "Something went wrong"
+          render 'new'
+        end
+    end
+    
+    
 end
